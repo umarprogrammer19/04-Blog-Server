1920b8270299459f2d75769aba48e97546566433
talhahzahid <talhahzahid415@gmail.com>
2024-12-20 18:41:36 +0500

    Like And Comment Done

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -3,4 +3,17 @@ import comment from '../models/comment.models.js'
 
 const userComment = async(req, res) => {
     const { userId, blogId, content } = req.body;
+    if (!userId || !blogId || !content) return res.status(400).json({ message: "all feilds are required" })
+    try {
+        const comments = await comment.create({ userId, blogId, content })
+        res.status(201).json({
+            message: "comment added successfully",
+            comments
+        })
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
 }
+
+
+export { userComment }
--- src/routes/reaction.routes.js
+++ src/routes/reaction.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { userLike } from "../controllers/like.controllers.js";
+import { userComment } from "../controllers/comment.controllers.js";
 
 
 
 const router = express.Router()
 
 router.post('/like', userLike)
+router.post('/comment', userComment)
 
 export default router
