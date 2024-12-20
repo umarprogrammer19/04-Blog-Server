fd5f8a6bb89dc79e77d0fb9a5afb2cc4a73f35bd
talhahzahid <talhahzahid415@gmail.com>
2024-12-20 12:33:11 +0500

    Blog Crud Compelete

--- src/controllers/blog.controllers.js
+++ src/controllers/blog.controllers.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
-
-
 import blog from '../models/blog.models.js'
 
-
+// add blog 
 const addBlog = async(req, res) => {
     const { title, description } = req.body;
     if (!title) return res.status(400).json({ message: "title is required" })
@@ -16,6 +14,7 @@ const addBlog = async(req, res) => {
     }
 }
 
+// delete blog 
 const deleteBlog = async(req, res) => {
     const { id } = req.params;
     if (!id) return res.status(400).json({ message: "id is required" })
@@ -28,6 +27,48 @@ const deleteBlog = async(req, res) => {
     }
 }
 
+// edit blog 
+const editBlog = async(req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    if (!id) return res.status(400).json({ message: "id is required" })
+    try {
+        const blogFound = await blog.findByIdAndUpdate(id, { title, description })
+        if (!blogFound) return res.status(404).json({ message: "no blog found !" })
+        res.status(200).json({ message: "blog edit successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "error occurred" })
+    }
+}
+
+// get all blog
+const allBlog = async(req, res) => {
+    try {
+        const blogs = await blog.find({})
+        res.status(200).json({ message: "fetch all blog", blogs })
+    } catch (error) {
+        res.status(500).json({
+            message: "error occurred",
+            error: error.message
+        })
+    }
+}
+
+// get single blog 
+const singleBlog = async(req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await blog.findById(id)
+        res.status(200).json({ message: user })
+    } catch (error) {
+        res.status(500).json({
+            message: "error occurred",
+            error: error.message
+        })
+    }
+}
 
 
-export { addBlog, deleteBlog }
+
+
+export { addBlog, deleteBlog, editBlog, allBlog, singleBlog }
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { addBlog, deleteBlog } from '../controllers/blog.controllers.js'
+import { addBlog, allBlog, deleteBlog, editBlog, singleBlog } from '../controllers/blog.controllers.js'
 
 const router = express.Router()
 
 router.post('/addblog', addBlog)
 router.delete('/delete/:id', deleteBlog)
+router.put('/edit/:id', editBlog)
+router.get('/blogs', allBlog)
+router.get('/blog/:id', singleBlog)
 
 
 export default router
