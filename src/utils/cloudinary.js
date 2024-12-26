9323e2529243d1001c292e9f58ced26fff34ee89
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-12-26 21:52:19 +0500

    Setup Cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadImageToCloudinary = async (localPath) => {
+    try {
+        const result = await cloudinary.uploader.upload(localPath, {
+            resource_type: "auto",
+        });
+        fs.unlinkSync(localPath);
+        return result.url;
+    } catch (err) {
+        return fs.unlinkSync(localPath);
+    };
+};
