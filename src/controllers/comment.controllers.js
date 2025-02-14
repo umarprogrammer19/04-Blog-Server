88b64ee9aa1d01439bbf5bb1b2b6c3221804f6c3
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-14 23:07:40 +0500

    Delete

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Blog from '../models/blog.models.js';
 import Comment from '../models/comment.models.js';
 
@@ -34,4 +35,32 @@ const userComment = async (req, res) => {
     }
 };
 
-export { userComment };
+const deleteComment = async (req, res) => {
+    if (!req.user) return res.status(400).json({ message: "Please Login First" });
+
+    const { commentId } = req.body;
+    if (!commentId) return res.status(400).json({ message: "Comment ID is required" });
+    if (!mongoose.Types.ObjectId.isValid(commentId)) return res.status(400).json({ message: "Invalid Comment ID" });
+
+    try {
+        // Find the comment
+        const comment = await Comment.findById(commentId);
+        if (!comment) return res.status(404).json({ message: "Comment Not Found" });
+
+        if (comment.userId.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "Unauthorized: You can only delete your own comment" });
+        }
+
+        await Comment.findByIdAndDelete(commentId);
+
+        await Blog.findByIdAndUpdate(comment.blogId, {
+            $pull: { comments: commentId }
+        });
+
+        res.status(200).json({ message: "Comment deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+export { userComment, deleteComment };
