9a890dcf6e29f1206e4dc01b470fbcb6d8cf7ce1
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-14 22:45:27 +0500

    Update Blog On Every Comment

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,25 +1,33 @@
-import comment from '../models/comment.models.js';
+import Blog from '../models/blog.models.js';
+import Comment from '../models/comment.models.js';
 
 const userComment = async (req, res) => {
     const { blogId, content } = req.body;
-    if (!req.user) return req.status(400).json({ message: "Please Login First" });
+    if (!req.user) return res.status(400).json({ message: "Please Login First" });
+
     const userId = req.user._id;
     if (!userId || !blogId || !content) {
         return res.status(400).json({ message: "All fields are required" });
     }
 
     try {
-        const comments = await comment.create({ userId, blogId, content });
+        // Create the comment
+        const newComment = await Comment.create({ userId, blogId, content });
 
-        const populatedComment = await comment
-            .findById(comments._id)
+        // Push the comment ID to the blog's comments array
+        await Blog.findByIdAndUpdate(blogId, {
+            $push: { comments: newComment._id }
+        });
+
+        // Populate the response
+        const populatedComment = await Comment
+            .findById(newComment._id)
             .populate('userId', 'email fullname')
-            .populate('blogId', 'title content');
+            .populate('blogId', 'title');
 
         res.status(201).json({
             message: "Comment added successfully",
-            comments,
-            populatedComment
+            comment: populatedComment
         });
     } catch (error) {
         return res.status(500).json({ message: "Server error", error: error.message });
