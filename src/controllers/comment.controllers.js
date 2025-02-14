9d3b419aea76b235233cda3898d2df424ca6f513
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-14 22:07:04 +0500

    Changes

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,20 +1,18 @@
 import comment from '../models/comment.models.js';
 
-const userComment = async(req, res) => {
+const userComment = async (req, res) => {
     const { userId, blogId, content } = req.body;
     if (!userId || !blogId || !content) {
         return res.status(400).json({ message: "All fields are required" });
     }
 
     try {
-        // Create the comment
         const comments = await comment.create({ userId, blogId, content });
 
-        // Populate userId and blogId references
         const populatedComment = await comment
-            .findById(comments._id) // Use 'comments._id' instead of 'comment._id'
-            .populate('userId', 'email') // Populate email from User model
-            .populate('blogId', 'title content'); // Populate title and content from Blog model
+            .findById(comments._id)
+            .populate('userId', 'email')
+            .populate('blogId', 'title content');
 
         res.status(201).json({
             message: "Comment added successfully",
